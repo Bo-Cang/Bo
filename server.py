08d43be8ef7264fe0fa5c644c1baefdb7c548adf
Bo-Cang <58157684+Bo-Cang@users.noreply.github.com>
2019-11-25 21:21:35 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 import bottle
 from bottle import route
+import os
+from datetime import datetime as dt
+from random import randrange
+
+from bottle import route, run, static_file, view
 @route('/')
 def index():
   return '<p>H123ello</p>'
 
-if __name__ == '__main__':
-  run(application=APP)
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
